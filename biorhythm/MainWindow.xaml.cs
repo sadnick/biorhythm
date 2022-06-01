0aa5a8cfd5eb1753ff17fd8ad55aaebf820c6583
sadnick <sadyrinnk@whatever.ru>
2022-06-01 12:10:17 +0300

    update1

--- biorhythm/MainWindow.xaml.cs
+++ biorhythm/MainWindow.xaml.cs
@@ -16,28 +16,82 @@ using System.Windows.Shapes;
 namespace biorhythm
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для MainView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
-            InitializeComponent();          
-        }
-
-        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
-        {
-
+            InitializeComponent();
         }
 
         private void CheckBox_Checked(object sender, RoutedEventArgs e)
         {
-
+            arbitrary.IsEnabled = true;
+        }
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            arbitrary.IsEnabled = false;
         }
 
-        private void Dates_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
+            List<operation> biorhythms = new List<operation>();
+            int arbitrarys = 0;
+            DateTime birthDate = DateTime.Now;
 
+            const int phys = 23;
+            const int emot = 28;
+            const int intel = 33;
+
+            try
+            {
+                birthDate = Convert.ToDateTime(BirthDate.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Произошла ошибка при получении даты рождения");
+            }
+
+            if (arbitrary.IsEnabled == true)
+            {
+                try
+                {
+                    arbitrarys = Convert.ToInt32(arbitrary.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("Произошла ошибка при получении данных отчета");
+                }
+            }
+            else
+            {
+                try
+                {
+                    arbitrarys = Convert.ToInt32(cmbCountdown.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("Произошла ошибка при получении данных отчета");
+                }
+            }
+            DateTime dateCountDown;
+            for (int i = 0; i < arbitrarys; i++)
+            {
+                dateCountDown = Convert.ToDateTime(countDownTime.Text);
+                var bior = new operation()
+                {
+                    Date = dateCountDown.AddDays(i).ToShortDateString(),
+                    Emotional = Math.Round((Math.Sin(2 * Math.PI * ((dateCountDown - birthDate).Days + i) / emot)) * 100, 2),
+                    Intellectual = Math.Round((Math.Sin(2 * Math.PI * ((dateCountDown - birthDate).Days + i) / intel)) * 100, 2),
+                    Physical = Math.Round((Math.Sin(2 * Math.PI * ((dateCountDown - birthDate).Days + i) / phys)) * 100, 2),
+                };
+                bior.Total = Math.Round(bior.Emotional + bior.Intellectual + bior.Physical, 2);
+
+                biorhythms.Add(bior);
+            }
+            Dates.ItemsSource = biorhythms;
         }
     }
 }
+
